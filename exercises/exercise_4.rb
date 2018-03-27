016a693f516405b0a86c8bc7723763128136bb8b
Greg Davis <gdavis111@live.ca>
2018-03-27 18:46:30 +0000

    Completed exercise 3 & 4, had issue with vagrant after 3 so didnt commit

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -6,4 +6,16 @@ require_relative './exercise_3'
 puts "Exercise 4"
 puts "----------"
 
-# Your code goes here ...
+Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true)
+
+@mens_stores.each { |store| puts "The #{store.name} location carries men's apparel and makes #{store.annual_revenue} in revenue each year." }
+
+@womens_stores = Store.where("womens_apparel = ? AND annual_revenue <= ?", true, 1000000)
+
+@womens_stores.each { |store| puts "The #{store.name} location carries women's apparel and made under $1,000,000 in annual revenue." }
+
+"created_at >= ? AND created_at <= ?"
